67c424f093b4dc0187696457176b9919769e1d42
Constantine Lignos <lignos@seas.upenn.edu>
2013-02-23 03:32:00 -0500

    Improve plot for DiBS scores.

--- analysis/dibs_analysis.R
+++ analysis/dibs_analysis.R
@@ -8,6 +8,6 @@ scale_x_log2 <- scale_x_continuous(trans = log2_trans(), breaks = trans_breaks('
 dibs <- read.csv('../data/br_dibs.csv')
 dibs$Boundary <- factor(dibs$Boundary, levels = c(TRUE, FALSE), labels = c("Word Boundary", "Word Internal"))
 
-pdf(file="dibs_stump_boundary.pdf", heigh = 3.5)
-ggplot(dibs, aes(Score + 0.01)) + geom_density(aes(fill = Boundary), alpha = 0.75) + scale_fill_grey() + scale_x_log2 + xlab("DiBS Score") + ylab("Density") +  geom_vline(xintercept = .16, linetype = "longdash", alpha = 0.7) + geom_vline(xintercept = .40, linetype = "longdash", alpha = 0.7) + theme_bw()
+pdf(file="dibs_stump_boundary.pdf", height = 4)
+ggplot(dibs, aes(Score + 0.01)) + geom_density(aes(fill = Boundary), alpha = 0.75) + scale_fill_grey() + scale_x_log2 + xlab("DiBS Score") + ylab("Density") +  geom_vline(xintercept = 1 / .16, linetype = "longdash", alpha = 0.7) + geom_vline(xintercept = 1 / .40, linetype = "longdash", alpha = 0.7)+ theme_bw() + theme(legend.position = "bottom")
 dev.off()
