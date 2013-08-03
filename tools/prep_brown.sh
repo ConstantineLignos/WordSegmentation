d4db14af1b693593ea9694377a135a0fddf098e9
Constantine Lignos <lignos@seas.upenn.edu>
2013-08-03 00:12:05 -0400

    Add auto-downloading of Brown data.

--- tools/prep_brown.sh
+++ tools/prep_brown.sh
@@ -1,30 +1,37 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # This script depends on having a number of other scripts that are not here,
 # all of which can be found at https://github.com/ConstantineLignos/LingTools
+# I just symlink these into this directory.
 # 1. syllabify.py (originally from https://github.com/kylebgorman/syllabify)
 # 2. clean_childes.py 
 # 3. The lexinfo package
 # Also, you need the following data:
-# 1. CHILDES data in CHA text (not XML) format
-# 2. A copy of CMUDict, preferably modified to remove stress from functional 
+# 1. A copy of CMUDict, preferably modified to remove stress from functional 
 #    elements
-# 
+# This will automatically download the Brown data from CHILDES.
+
+DATA=../data
+
+# Download and unzip the Brown data if needed
+if [ ! -d $DATA/Brown ]; then
+    wget http://childes.psy.cmu.edu/data/Eng-NA-MOR/Brown.zip -O $DATA/Brown.zip
+    unzip -o $DATA/Brown.zip -d $DATA
+fi
 
 # In one transcript, Sarah is SAR instead of CHI
 FILTER="^CHI,SAR"
-ROOT=../data
 for NAME in adam eve sarah
 do
     echo "Processing ${NAME}..."
-    CAT_CHA=$ROOT/${NAME}.cha
-    CAT_CLEAN=$ROOT/${NAME}.txt
-    SYLL=$ROOT/${NAME}_syll.txt
+    CAT_CHA=$DATA/${NAME}.cha
+    CAT_CLEAN=$DATA/${NAME}.txt
+    SYLL=$DATA/${NAME}_syll.txt
     echo "Concatenating to ${CAT_CHA}"
-    cat childes_data/Brown/"${NAME^}"/${NAME}*.cha > $CAT_CHA
+    cat $DATA/Brown/"${NAME^}"/${NAME}*.cha > $CAT_CHA
     ./clean_childes.py $CAT_CHA $CAT_CLEAN $FILTER clean
     echo "Converting to phonemic form"
-    ./convert_corpus_phonemic.py $ROOT/cmudict.0.7a_ext_reduced eng < $CAT_CLEAN > $SYLL 2> /dev/null
+    ./convert_corpus_phonemic.py $DATA/cmudict.0.7a_ext_reduced eng < $CAT_CLEAN > $SYLL
     echo "Number of lines:"
     wc -l < $SYLL
     echo "Unique words:"
