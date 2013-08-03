b6f25472e44ddd7175130ac0b71b03ae10f86ffc
Constantine Lignos <lignos@seas.upenn.edu>
2013-08-03 17:09:59 -0400

    Add convenience scripts for running evaluations.

---
+++ cats/runexp-all.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./segexperiment.sh ../data/all_syll_train.txt ../data/all_syll_test.txt out/exp propslist.txt exp.csv
---
+++ cats/runexp-gy.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./segexperiment.sh ../data/all_syll_train.txt ../data/all_syll_test.txt out/gy gy_propslist.txt gy.csv
