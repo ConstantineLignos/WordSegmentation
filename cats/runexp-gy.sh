b8f9aa6f9c85902706ce338a92406da91071e49c
Constantine Lignos <lignos@seas.upenn.edu>
2013-08-08 03:12:01 -0400

    Add unreal stress to GY experiments.

--- cats/runexp-gy.sh
+++ cats/runexp-gy.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 ./segexperiment.sh ../data/all_syll_train.txt ../data/all_syll_test.txt out/gy gy_propslist.txt gy.csv
+./segexperiment.sh ../data/all_syll_unreal_train.txt ../data/all_syll_unreal_test.txt out/gyunreal gy_propslist.txt gy_unreal.csv
